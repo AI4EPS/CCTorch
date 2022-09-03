f4f07eed034074bce1df208ddf7fddab137956df
Weiqiang <zhuwq0@gmail.com>
2022-09-02 17:21:09 -0700

    fix torchvision

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="CCTorch",
-    version="0.1.0",
+    version="0.1.1",
     long_description="Cross-Correlation using Pytorch",
     long_description_content_type="text/markdown",
     packages=["cctorch"],
-    install_requires=["torch", "torch-vision", "h5py", "matplotlib", "pandas"],
+    install_requires=["torch", "torchvision", "h5py", "matplotlib", "pandas"],
 )
