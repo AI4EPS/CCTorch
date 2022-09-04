135b5f8442e2bf870990381b712303150f44a04d
matrix137 <lijiaxuanmail@gmail.com>
2022-09-04 16:53:36 -0700

    fft and normalize

--- cctorch/transforms.py
+++ cctorch/transforms.py
@@ -13,29 +13,17 @@ def nextpow2(i):
     return n
 
 
-def FFT(x):
+def fft_real(x):
     """
     assume fft axis in dim=-1
     """
     ntime = x.shape[-1]
-    cclag = xcorr_lag(ntime)
-    nxcor = len(cclag)
-    nfast = nextpow2(nxcor)
+    nfast = nextpow2(2*ntime-1)
     return torch.fft.rfft(x, n=nfast, dim=-1)
 
 
-def FFT_NORMALIZE(x):
+def fft_normalize(x):
     """"""
-    x[:] = FFT(x)
-    mean = torch.mean(x, dim=-1, keepdims=True)
-    std = torch.std(x, dim=-1, keepdims=True, unbiased=False)
-    x[:] = ((x-mean) / std) / torch.sqrt(torch.Tensor([x.shape[-1]]))
-    return x
-
-
-def MA(x, nma=20):
-    """
-    Moving average in dim=-1
-    """
-    m = torch.nn.AvgPool1d(nma, stride=1, padding=nma // 2)
-    return m(x.transpose(1, 0))[:, : x.shape[0]].transpose(1, 0)
+    x -= torch.mean(x, dim=-1, keepdims=True)
+    x /= x.square().sum(dim=-1, keepdims=True).sqrt()
+    return fft_real(x)
