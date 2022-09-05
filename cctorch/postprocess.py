4931b22a82c4bc21d9f9fb44ebded72a30fbf3d5
matrix137 <lijiaxuanmail@gmail.com>
2022-09-04 23:15:07 -0700

    write when no automatic batch

--- cctorch/postprocess.py
+++ cctorch/postprocess.py
@@ -1,10 +1,15 @@
 import pandas as pd
+import numpy as np
 
 
 def write_xcor_to_csv(result, path_result):
-    nbatch = len(result["id1"])
-    id1 = result["id1"].cpu().numpy()
-    id2 = result["id2"].cpu().numpy()
+    nbatch = result["cc"].shape[0]
+    if np.isscalar(result["id1"]):
+        id1 = [result["id1"]]
+        id2 = [result["id2"]]
+    else:
+        id1 = result["id1"].cpu().numpy()
+        id2 = result["id2"].cpu().numpy()
     cc = result["cc"].cpu().numpy()
     dt = result["dt"].cpu().numpy()
     for ibatch in range(nbatch):
