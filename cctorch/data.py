342cfd3de2e8b503e77cc7038c6cd99e59d67dba
matrix137 <lijiaxuanmail@gmail.com>
2022-10-10 17:48:10 -0700

    data.py: read pair from text file with 3 options

--- cctorch/data.py
+++ cctorch/data.py
@@ -56,10 +56,11 @@ class CCDataset(Dataset):
 class CCIterableDataset(IterableDataset):
     def __init__(
         self,
-        data_list1,
-        data_list2,
-        block_size1,
-        block_size2,
+        pair_list,
+        generate_pair,
+        auto_xcor,
+        block_num1,
+        block_num2,
         data_path,
         shared_dict,
         device="cpu",
@@ -69,16 +70,17 @@ class CCIterableDataset(IterableDataset):
         **kwargs,
     ):
         super(CCIterableDataset).__init__()
-        self.data_list1 = data_list1
-        self.data_list2 = data_list2
-        self.group1 = [list(x) for x in np.array_split(data_list1, len(data_list1) // block_size1)]
-        self.group2 = [list(x) for x in np.array_split(data_list2, len(data_list2) // block_size2)]
-        self.block_size1 = block_size1
-        self.block_size2 = block_size2
+        self.generate_pair = generate_pair
+        self.auto_xcor = auto_xcor
+        self.pairs, self.data_list1, self.data_list2 = self._read_pair(pair_list)
+        self.group1 = [list(x) for x in np.array_split(self.data_list1, block_num1) if len(x)>0]
+        self.group2 = [list(x) for x in np.array_split(self.data_list2, block_num2) if len(x)>0]
+        self.block_num1 = len(self.group1)
+        self.block_num2 = len(self.group2)
         # if len(self.group1) > len(self.group2):
         #     self.group1, self.group2 = self.group2, self.group1
         #     self.block_size1, self.block_size2 = self.block_size2, self.block_size1
-        self.block_index = [(i, j) for i in range(len(self.group1)) for j in range(i, len(self.group2))][
+        self.block_index = [(i, j) for i in range(len(self.group1)) for j in range(len(self.group2))][
             rank::world_size
         ]
         self.data_path = Path(data_path)
@@ -103,14 +105,57 @@ class CCIterableDataset(IterableDataset):
         #return iter(self.sample(self.block_index))
         return iter(self.sample(self.block_index[worker_id::num_workers]))
 
+    def _read_pair(self, file_pair_list):
+        # read pair ids from a text file
+        df_pair = pd.read_csv(file_pair_list, header=None)
+        ncol = len(df_pair.columns)
+        if ncol == 1:
+            # if the text file contains only one column, generate all possible pairs
+            df_pair.rename(columns={0:"event1"}, inplace=True)
+            data_list1 = sorted(list(set(df_pair["event1"].tolist())))
+            data_list2 = data_list1
+            pairs = self._generate_pair_set(data_list1, data_list2)
+        elif ncol == 2:
+            df_pair.rename(columns={0:"event1", 1:"event2"}, inplace=True)
+            data_list1 = sorted(list(set(df_pair["event1"].tolist())))
+            data_list2 = sorted(list(set(df_pair["event2"].tolist())))
+            if self.generate_pair:
+                # if the text file contains two columns and generate=True, generate all possible pairs
+                pairs = self._generate_pair_set(data_list1, data_list2)
+            else:
+                # if the text file contains two columns and generate=False, only use pairs in the text file
+                pairs = set((row["event1"], row["event2"]) for _, row in df_pair.iterrows())
+        return pairs, data_list1, data_list2
+
+    def _generate_pair_set(self, event1, event2):
+        # generate set of all pairs
+        if self.auto_xcor:
+            xcor_offset = 0
+        else:
+            xcor_offset = 1
+        event_inner = set(event1) & set(event2)
+        event_outer1 = [evt for evt in event1 if evt not in event_inner]
+        event_outer2 = [evt for evt in event2 if evt not in event_inner]
+        event_inner = sorted(list(event_inner))
+        pairs = {*()}
+        if len(event_inner) > 0:
+            pairs.update(set([(evt1, evt2) for i1, evt1 in enumerate(event_inner) for evt2 in event_inner[i1+xcor_offset:]]))
+            if len(event_outer1) > 0:
+                pairs.update(set([(evt1, evt2) for evt1 in event_outer1 for evt2 in event_inner]))
+            if len(event_outer2) > 0:
+                pairs.update(set([(evt1, evt2) for evt1 in event_inner for evt2 in event_outer2]))
+        if len(event_outer1) > 0 and len(event_outer2) > 0:
+                pairs.update(set([(evt1, evt2) for evt1 in event_outer1 for evt2 in event_outer2]))
+        return pairs
+
     def _read_das(self, event, local_dict):
 
         if event not in local_dict:
             data_list, info_list = read_das_eventphase_data_h5(self.data_path / f"{event}.h5", phase="P", event=True, dataset_keys=["shift_index"])
-            data = torch.from_numpy(data_list[0])
+            data = torch.tensor(data_list[0], device=self.device)
             info = info_list[0]
             if self.transform is not None:
-                local_dict[event] = (self.transform(data.cuda()), info)
+                local_dict[event] = (self.transform(data), info)
 
         return local_dict[event]
 
@@ -124,8 +169,10 @@ class CCIterableDataset(IterableDataset):
             event1, event2 = self.group1[i], self.group2[j]
             # print(f"{len(event1) = }, {len(event2) = }")
             for ii in range(len(event1)):
-                begin = ii if i == j else 0
-                for jj in range(begin, len(event2)):
+                # begin = ii if i == j else 0
+                for jj in range(len(event2)):
+                    if (event1[ii], event2[jj]) not in self.pairs:
+                        continue
                     # print(f"{i=}, {j=}, {ii=}, {jj=} {event1[ii]=}, {event2[jj]=}")
                     data_tuple1 = self._read_das(event1[ii], local_dict)
                     data_tuple2 = self._read_das(event2[jj], local_dict)
@@ -133,11 +180,13 @@ class CCIterableDataset(IterableDataset):
                         {"event": event2[jj], "data": data_tuple2[0], "event_time": data_tuple2[1]["event"]["event_time"], "shift_index": data_tuple2[1]["shift_index"]}
             
             del local_dict
-            torch.cuda.empty_cache()
+            if self.device == "cuda":
+                torch.cuda.empty_cache()
 
     def __len__(self):
-        short_list = min(len(self.data_list1), len(self.data_list2))
-        return len(self.data_list1) * len(self.data_list2) - short_list * (short_list + 1) // 2
+        #short_list = min(len(self.data_list1), len(self.data_list2))
+        #return len(self.data_list1) * len(self.data_list2) - short_list * (short_list + 1) // 2
+        return len(self.pairs)
 
 
 # helper reading functions
