a8aa4017dafb8e449b9e0a7037554e23275a08ea
Weiqiang Zhu <zhuwq0@gmail.com>
2025-05-12 10:32:48 -0700

    increase block size

--- examples/california/run_cctorch.py
+++ examples/california/run_cctorch.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
         print(cmd)
         os.system(cmd)
 
-        cmd = f"python /opt/CCTorch/run.py --pair_list=pairs2_{year}_{jday}.txt --data_list1=mseeds2_{year}_{jday}.txt --data_format1=mseed --sampling_rate=20 --mode=AN  --block_size1 50 --block_size2 50 --batch_size 1  --domain stft --device=cuda"
+        cmd = f"python /opt/CCTorch/run.py --pair_list=pairs2_{year}_{jday}.txt --data_list1=mseeds2_{year}_{jday}.txt --data_format1=mseed --sampling_rate=20 --mode=AN  --block_size1 300 --block_size2 300 --batch_size 1  --domain stft --device=cuda"
         # cmd = f"python ../../run.py --pair_list=pairs2_{year}_{jday}.txt --data_list1=mseeds2_{year}_{jday}.txt --data_format1=mseed --sampling_rate=20 --mode=AN  --block_size1 10 --block_size2 10 --batch_size 1  --domain stft --device=cpu"
         print(cmd)
         os.system(cmd)
